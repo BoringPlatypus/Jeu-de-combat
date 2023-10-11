30f666b10d04f0d890a015247f812cc5eda6247f
Milo Trendel <Milo Trendel@LAPTOP-2L86F3PO>
2023-10-11 15:18:45 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world");
+    }
+}
